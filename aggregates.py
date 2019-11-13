ddfe68261de4a52a6e556faa48d48f74afb9cb95
Joseph Montoya <joseph.montoya@tri.global>
2019-11-13 15:40:27 -0800

    update names

---
+++ aggregates.py
@@ -0,0 +1,28 @@
+import abc
+from .table import ParameterTable, HashedParameterArray
+from .events import ParametersAdded
+
+
+class Aggregate(abc.ABC):
+    @classmethod
+    @abc.abstractmethod
+    def from_events(cls, events):
+        pass
+
+
+class ParameterTables(Aggregate):
+    @classmethod
+    def from_events(cls, events):
+        pass
+
+
+# More lightweight aggregate with just the names
+class ParameterTableNames(HashedParameterArray, Aggregate):
+    def __init__(self, parameter_table_names):
+        super(ParameterTableNames, self).__init__(parameter_table_names)
+
+    @classmethod
+    def from_events(cls, events):
+        add_parameter_events = filter(lambda x: isinstance(x, ParametersAdded), events)
+        parameter_table_names = [event.table_name for event in add_parameter_events]
+        return cls(parameter_table_names)
--- events.py
+++ events.py
@@ -2,6 +2,7 @@ import abc
 from datetime import datetime
 from monty.json import MSONable
 
+
 class Event(abc.ABC):
     def __init__(self, time=None):
         self._time = time or datetime.utcnow().isoformat()
@@ -10,28 +11,28 @@ class Event(abc.ABC):
 # TODO: more descriptive name, populate space?
 class ParametersAdded(Event, MSONable, time=None):
     def __init__(self, table_name, parameters, time):
-        self._table_name = table_name
-        self._parameters = parameters
+        self.table_name = table_name
+        self.parameters = parameters
         super(Event, self).__init__(time)
 
     def as_dict(self):
         return {
-            "table_name": self._table_name,
-            "parameters": self._parameters,
+            "table_name": self.table_name,
+            "parameters": self.parameters,
             "time": self._time
         }
 
 
 class MethodAdded(Event, MSONable):
     def __init__(self, name, parameter_names, time=None):
-        self._name = name
-        self._parameter_names = parameter_names
+        self.name = name
+        self.parameter_names = parameter_names
         super(Event, self).__init__(time)
 
     def as_dict(self):
         return {
-            "name": self._name,
-            "parameters": self._parameter_names,
+            "name": self.name,
+            "parameters": self.parameter_names,
             "time": self._time
         }
 
