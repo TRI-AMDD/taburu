28c4c41a0895ba4772676ce8b0d661f5de9102bf
Joseph Montoya <joseph.montoya@tri.global>
2019-11-14 10:58:22 -0800

    add setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='taburu',
+    version='0.1',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='joseph.montoya',
+    author_email='joseph.montoya@tri.global',
+    description='',
+    install_requires=["monty",
+                      "indexed.py",
+                      "tqdm",
+                      ],
+)
